db31b4679e69178ce4fd32c4c7eecf77d4d879dc
hecuhecu <hecunyuji5@gmail.com>
2024-03-02 01:52:03 +0900

    Initial Commit

---
+++ qiita-app/qiita_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  qiita_appApp.swift
+//  qiita-app
+//
+//  Created by Hiroki Kawamura on 2024/03/02.
+//
+
+import SwiftUI
+
+@main
+struct qiita_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
