d9da23a885fed158a489e587f537b1b683d1eca1
hecuhecu <hecunyuji5@gmail.com>
2024-03-09 23:41:00 +0900

    manage view status by data acquisition results via api

--- qiita-app/ViewModels/ArticleListViewModel.swift
+++ qiita-app/ViewModels/ArticleListViewModel.swift
@@ -1,16 +1,33 @@
 import Foundation
 
+enum DataState {
+    case idle
+    case loading
+    case loaded(Result<[Article], Error>)
+    
+    var isLoading: Bool {
+        if case .loading = self {
+            return true
+        }
+        
+        return false
+    }
+}
+
 class ArticleListViewModel: ObservableObject {
-    @Published var articles: [Article] = []
+    @Published var dataState: DataState = .idle
 
     func fetchArticles() {
+        if dataState.isLoading { return }
+        dataState = .loading
+        
         APIService.shared.fetchArticles { result in
             DispatchQueue.main.async {
                 switch result{
                 case .success(let fetchedArticles):
-                    self.articles = fetchedArticles
+                    self.dataState = .loaded(.success(fetchedArticles))
                 case .failure(let error):
-                    print("error: \(error.localizedDescription)")
+                    self.dataState = .loaded(.failure(error))
                 }
             }
         }
--- qiita-app/Views/ArticleListView.swift
+++ qiita-app/Views/ArticleListView.swift
@@ -4,18 +4,34 @@ struct ArticleListView: View {
     @ObservedObject var viewmodel = ArticleListViewModel()
     
     var body: some View {
-        NavigationStack {
-            List(viewmodel.articles) { article in
-                NavigationLink(destination: 
-                                ArticleDetailView(article: article)) {
-                    ArticleRowView(article: article)
+        Group {
+            switch viewmodel.dataState {
+            case .idle, .loading:
+                EmptyView()
+            case .loaded(.success(let articles)):
+                NavigationStack {
+                    List(articles) { article in
+                        NavigationLink(destination:
+                                        ArticleDetailView(article: article)) {
+                            ArticleRowView(article: article)
+                        }
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                }
+            case .loaded(.failure):
+                ErrorStateView {
+                    viewmodel.fetchArticles()
                 }
             }
-            .navigationTitle("Home")
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear {
-                viewmodel.fetchArticles()
+        }
+        .overlay {
+            if viewmodel.dataState.isLoading {
+                ProgressView()
             }
+        }
+        .onAppear {
+            viewmodel.fetchArticles()
         }
     }
 }
