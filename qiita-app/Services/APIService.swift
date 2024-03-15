d1a6fe8170427d01dd2cfebac5e264bdbf5aa137
hecuhecu <hecunyuji5@gmail.com>
2024-03-15 22:50:46 +0900

    use Alamofire

--- qiita-app/Services/APIService.swift
+++ qiita-app/Services/APIService.swift
@@ -1,20 +1,19 @@
 import Foundation
+import Alamofire
 
 final class APIService {
     
     public static let shared = APIService()
-    private let baseURLString = "https://qiita.com/api/v2/items?"
     
     public func fetchArticles() async throws -> [Article] {
-        let totalURLString = baseURLString + "per_page=100"
-        let url = URL(string:  totalURLString)
-        guard let url = url else { return [] }
-        let request = URLRequest(url: url)
+        let baseUrlString = "https://qiita.com/api/v2/items?"
+        let searchPath = "per_page=100"
+        let urlString = baseUrlString + searchPath
         
-        let (data, response) = try await URLSession.shared.data(for: request)
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let fetchedArticles = try decoder.decode([Article].self, from: data)
-        return fetchedArticles
+        let dataTask = AF.request(urlString).serializingDecodable([Article].self, decoder: decoder)
+        let value = try await dataTask.value
+        return value
     }
 }
